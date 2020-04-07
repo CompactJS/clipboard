71106a19a707a4fa7b620bcd69aa9a45577fd393
TimoBechtel <TimoBechtel@users.noreply.github.com>
2020-04-07 18:06:50 +0000

    deploy: dd9e561d04e148657ffa46bdfbc6ce7782827a6d

---
+++ dist/index.module.js
@@ -0,0 +1,2 @@
+var e=function(e){var t=e;"string"==typeof e&&((t=document.createElement("textarea")).style="position:fixed;top:0;left:0;",t.value=e,document.body.appendChild(t)),t.select(),document.execCommand("copy"),"string"==typeof e&&document.body.removeChild(t)};export{e as clipboard};
+//# sourceMappingURL=index.module.js.map
